3a4fed3224816e9505e8e5b3599f9e5cce340733
Adam.B3 <readingfootyclub1871@gmail.com>
2023-10-27 14:12:24 +0100

    basic setup

--- src/main/java/com/lbg/App.java
+++ src/main/java/com/lbg/App.java
@@ -1,9 +1,62 @@
 package com.lbg;
 
-public class App 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class App
 {
+    final private WebDriver driver = new ChromeDriver();
+
     public static void main( String[] args )
     {
-        /*Code here*/
+
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments();
+        //invoke the browser
+
+        WebDriver driver = new ChromeDriver();
+
+        //
+        driver.get("https://mdbootstrap.com/docs/standard/extended/login/");
+
+        String title = driver.getTitle();
+        System.out.println("Title is: "+title);
+
+        String url= driver.getCurrentUrl();
+        System.out.println("URL is: " + url);
+
+//        driver.manage().window().maximize();
+
+        driver.findElement(By.id("form2Example1")).sendKeys("adam");
+        WebElement but = driver.findElement(By.className("btn btn-primary btn-block mb-4"));
+        but.click();
+
+//        driver.close();
+//
+//        driver.quit(); // fully shuts down the webdriver
+
+    }
+
+    public void login(){
+        driver.manage().window().maximize();
+        driver.get("https://www.browserstack.com/users/sign_in");
+        WebElement username=driver.findElement(By.id("user_email_login"));
+        WebElement password=driver.findElement(By.id("user_password"));
+        WebElement login=driver.findElement(By.name("commit"));
+        username.sendKeys("abc@gmail.com");
+        password.sendKeys("your_password");
+        login.click();
+
+        // 1) actual url
+        // 2) expected url
+        // 3) assert if the expected match the current
+    }
+
+    public WebDriver getDriver() {
+
+        return driver;
     }
 }
--- src/test/java/com/lbg/AppTest.java
+++ src/test/java/com/lbg/AppTest.java
@@ -1,12 +1,28 @@
 package com.lbg;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
 
     @Test
     void main() {
+
+    }
+
+    @Test
+    void login() {
+        App app = new App();
+        WebDriver driver= app.getDriver();
+        app.login();
+
+        String actualURL = driver.getCurrentUrl();
+        String expectedURL = "https://live.browserstack.com/dashboard";
+        assertEquals(expectedURL,actualURL);
 
     }
 }
